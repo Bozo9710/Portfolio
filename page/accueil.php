9d33d49efae84d1f6e82ccbfa85c41ce2b6e6b84
Lothaire Quintard <lquintard@info.local>
2024-11-27 17:02:41 +0100

    Accueil début

---
+++ page/accueil.php
@@ -0,0 +1,47 @@
+<?php
+    // Inclure le header
+ //   include('includes/header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Portfolio</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <!-- En-tête avec navigation -->
+    <header>
+        <nav>
+            <ul>
+                <li><a href="accueil.php">Accueil</a></li>
+                <li><a href="competences.php">Compétences</a></li>
+                <li><a href="realisations.php">Réalisations</a></li>
+                <li><a href="formations.php">Formations</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Section présentation de tes projets -->
+    <section id="projets">
+        <h1></h1>
+        <div class="acceuil">
+            <?php
+            require_once("../yaml/yaml.php");
+            $data=yaml_parse_file('../data/accueil.yaml');
+            echo $data["Prenom"]." ".$data["Nom"]."<br>";
+            echo '<img src="../image/'.$data["Image1"].'"><br> ';
+            echo $data["Presentation"];              
+            ?>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <?php // include('includes/footer.php'); ?>
+
+</body>
+</html>
