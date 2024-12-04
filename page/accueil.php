78943a1da1428bfa1bc9063d8aab426ec4c69916
Lothaire <lothairre@gmail.com>
2024-12-04 19:30:17 +0100

    19h30 4/12 session de 2h30

--- page/accueil.php
+++ page/accueil.php
@@ -1,18 +1,12 @@
-<?php
-    // Inclure le header
- //   include('includes/header.php');
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil Portfolio LOTHAIRE </title>
-    <link rel="stylesheet" href="./css/accueil.css">
+    <title>Accueil Portfolio LOTHAIRE</title>
+    <link rel="stylesheet" href="./../css/accueil.css">
 </head>
 <body>
-
     <!-- En-tête avec navigation -->
     <header>
         <nav>
@@ -25,25 +19,47 @@
             </ul>
         </nav>
     </header>
+    <?php
+    require_once("../yaml/yaml.php");
+    $data=yaml_parse_file('../data/accueil.yaml');?>
 
     <!-- Section présentation de tes projets -->
     <section id="projets">
-        <h1></h1>
-        <div class="acceuil">
-            <?php
-            require_once("../yaml/yaml.php");
-            $data=yaml_parse_file('../data/accueil.yaml');
-            echo $data["Prenom"]." ".$data["Nom"]."<br>";
-            echo '<img src="../image/'.$data["PDP"].'"><br> '; 
-            echo $data["Date"] ;  
-            echo $data["Titre2"];
-
-            ?>
+    
+    <div class="bulle-container">
+        <!-- Partie 1 -->
+        <div class="bulle" id="bulle1">
+            <h2><?php echo $data["Prenom"] . " " . $data["Nom"]; ?></h2>
+            <img src="../image/<?php echo $data["PDP"]; ?>" alt="Photo de profil" class="pdp">
+            <p class="date"><?php echo $data["Date"]; ?></p>
         </div>
-    </section>
+        <!-- Partie 2 -->
+        <div class="bulle" id="bulle2">
+            <p class="mid"><?php echo $data["Titre2"]; ?></p>
+
+            <p class="textA"><?php echo $data["TextA"]; ?></p>
+
+            <img src="../image/<?php echo $data["ImageA"]; ?>" alt="Image A" class="image">
+
+            <p class="textB"> <?php echo $data["TextB"]; ?></p>
+            <img src="../image/<?php echo $data["ImageB"]; ?>" alt="Image B" class="image">
+
+            <p class="textC"> <?php echo $data["TextC"]; ?></p>
+            <img src="../image/<?php echo $data["ImageC"]; ?>" alt="Image C" class="image">
+        </div>
+
+        <!-- Partie 3 -->
+        <div class="bulle" id="bulle3">
+            <p class="right"><?php echo $data["Titre3"]; ?></p>
+            
+            <p class="texte3"><?php echo $data["Texte"]; ?></p>
+        </div>
+    </div>
+        
+</section>
+
 
     <!-- Footer -->
     <?php // include('includes/footer.php'); ?>
-
 </body>
 </html>
